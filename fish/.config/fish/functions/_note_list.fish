d48ed20b990d3b636db4ff0258e4a67669755743
Jaeho Cho <jaeho2025@gmail.com>
2026-04-10 17:38:00 -0400

    chore(fish): remove note functions

--- fish/.config/fish/functions/_note_list.fish
+++
@@ -1,7 +0,0 @@
-function _note_list --description "List notes with content for fzf input"
-    set -l notes_dir ~/notes
-    for f in (command ls $argv $notes_dir/*.md 2>/dev/null)
-        set -l content (string join ' ' < $f | string replace -ra '\s+' ' ' | string sub -l 1000)
-        printf '%s\x1f%s\x1f%s\n' $f (basename $f) "$content"
-    end
-end
